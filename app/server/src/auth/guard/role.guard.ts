3c6a385bbbdf33bce180f885f6490f21037108c4
风风 <qwe18173945756@163.com>
2026-05-16 23:09:17 +0800

    完成了token和权限的守卫

---
+++ app/server/src/auth/guard/role.guard.ts
@@ -0,0 +1,41 @@
+
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { User } from "@prisma/client";
+import { PrismaService } from '@/prisma/prisma.service';
+import { RoleWeight,Role } from "@/common/constants"
+import { Request } from 'express';
+
+interface CustomRequest extends Request {
+  user: User;
+}
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector,private prisma: PrismaService) { }
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean>  {
+    //获取用户的信息 ，其信息的来源为jwt守卫 ，其数据储存在 Request 对象中 
+    const req = context.switchToHttp().getRequest<CustomRequest>(); //switchToHttp 用于切换到http服务
+    const reqMethod = req.method;
+    let teamId:number;
+    if(reqMethod.toLowerCase() === "post"){
+        teamId = req.body.id;
+    }else{
+        teamId = +req.params.id;
+    }
+    const user = req.user ;
+    const methodRoles = this.reflector.get<Role>('roles', context.getHandler())
+    const classRoles = this.reflector.get<Role>('roles', context.getHandler())
+    const { role } = await this.prisma.teamMember.findFirst({
+        where:{
+            userId:user.id,
+            teamId,
+        }
+    })
+    let needWeight = Math.max(RoleWeight[methodRoles],RoleWeight[classRoles])
+    return RoleWeight[role] >= needWeight;
+
+  }
+}
