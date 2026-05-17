5e2bb36541952d0f036bc086e6a7165057a41ba3
风风 <qwe18173945756@163.com>
2026-05-17 22:05:46 +0800

    添加了转换函数，用于将请求体中id相关的参数类型转为number

---
+++ app/server/src/utils/idToNum.ts
@@ -0,0 +1,22 @@
+
+
+type dataType = {
+    [key:string]:string
+}
+type resType = {
+    [key:string] :number
+}
+export function idToNum(data:dataType):resType{
+    let res = {}
+    for (const key in data) {
+        if (!data.hasOwnProperty(key)) continue;
+        if(key.toLowerCase().includes("id")){
+            let value = data[key]
+            res[key] = +value;
+        }
+        
+        
+        
+    }
+    return res;
+}
