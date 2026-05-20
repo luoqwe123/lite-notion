93670127dcbe57fbcbc9b8ff437ae88a1f617521
风风 <qwe18173945756@163.com>
2026-05-20 20:39:25 +0800

    白天黑夜主题切换添加过渡动画

--- app/web/src/stores/modules/themeStore.ts
+++ app/web/src/stores/modules/themeStore.ts
@@ -8,10 +8,44 @@ export const useThemeStore = defineStore('theme', {
     currentTheme: 'light' as Theme,
   }),
   actions: {
-    toggleTheme() {
+    toggleTheme(e:MouseEvent) {
+      
       this.currentTheme = this.currentTheme === 'light' ? 'dark' : 'light';
       localStorage.setItem('theme', this.currentTheme); // 持久化
-      document.documentElement.classList.toggle('dark', this.currentTheme === 'dark');
+      const transition = document.startViewTransition(() => {
+        // 在 startViewTransition 中修改 DOM 状态产生动画
+        document.documentElement.classList.toggle('dark', this.currentTheme === 'dark');
+      })
+      transition.ready.then(() => {
+        // 由于我们要从鼠标点击的位置开始做动画，所以我们需要先获取到鼠标的位置
+        const { clientX, clientY } = e
+
+        // 计算半径，以鼠标点击的位置为圆心，到四个角的距离中最大的那个作为半径
+        const radius = Math.hypot(
+          Math.max(clientX, innerWidth - clientX),
+          Math.max(clientY, innerHeight - clientY)
+        )
+        const clipPath = [
+          `circle(0% at ${clientX}px ${clientY}px)`,
+          `circle(${radius}px at ${clientX}px ${clientY}px)`
+        ]
+
+        const isDark = document.documentElement.classList.contains('dark')
+        // 自定义动画
+        document.documentElement.animate(
+          {
+            clipPath: isDark ? clipPath.reverse() : clipPath
+          },
+          {
+            duration: 500,
+            pseudoElement: isDark
+              ? '::view-transition-old(root)'
+              : '::view-transition-new(root)'
+          }
+        )
+      })
+
+
     },
     initializeTheme() {
       const savedTheme = localStorage.getItem('theme') as Theme;
